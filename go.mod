c939e795639bbc905c57b2810b56aedbf3cb48e5
profy3232 <profy3232@gmail.com>
2025-07-02 02:32:30 +0300

    feat(internal): Add logging and input validation utilities

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/chai2010/webp v1.4.0
 	github.com/fatih/color v1.18.0
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/pflag v1.0.6 // indirect
 	golang.org/x/image v0.28.0
 	gopkg.in/yaml.v3 v3.0.1
---
+++ lib/internal/validator/validator.go
@@ -0,0 +1,95 @@
+package validator
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"syscall"
+)
+
+type ValidationError struct {
+	Field   string
+	Message string
+}
+
+// Error implements the error interface.
+func (e *ValidationError) Error() string {
+	return fmt.Sprintf("%s: %s", e.Field, e.Message)
+}
+
+// ValidetInput validates the input directory and target format.
+// It checks if the input directory is provided, exists, and has read permission.
+// It also verifies if the target format is within the supported formats.
+// Returns a ValidationError if any validation fails, otherwise returns nil.
+
+func ValidetInput(inputDirectory, targetFormat string, supportedFormatschan []string) error {
+
+	if inputDirectory == "" {
+		return &ValidationError{"inputDir", "input directory is required"}
+	}
+
+	if _, err := os.Stat(inputDirectory); os.IsNotExist(err) {
+		return &ValidationError{
+			Field:   "inputDirectory",
+			Message: fmt.Sprintf("input directory %s does not exist", inputDirectory),
+		}
+	}
+
+	if !hasReadPermission(inputDirectory) {
+		return &ValidationError{
+			Field:   "inputDirectory",
+			Message: fmt.Sprintf("input directory %s does not have read permission", inputDirectory),
+		}
+	}
+
+	if !isValidFormat(targetFormat, supportedFormatschan) {
+		return &ValidationError{
+			Field:   "targetFormat",
+			Message: fmt.Sprintf("target format %s is not supported", targetFormat),
+		}
+	}
+	return nil
+}
+
+// ValidateFilePath checks if the given path is valid and does not contain any path traversal.
+// Returns an error if the path is invalid, otherwise returns nil.
+func ValidateFilePath(path string) error {
+	cleanPath := filepath.Clean(path)
+	if strings.Contains(cleanPath, "..") {
+		return fmt.Errorf("invalid path (contains path traversal): %s", path)
+	}
+	return nil
+}
+func HasSufficientSpace(dir string, requiredBytes int64) bool {
+    var stat syscall.Statfs_t
+    if err := syscall.Statfs(dir, &stat); err != nil {
+        return false
+    }
+    return int64(stat.Bavail)*int64(stat.Bsize) > requiredBytes
+}
+
+
+// hasReadPermission checks if the specified path can be opened for reading.
+// Returns true if the path can be opened, otherwise returns false.
+
+func hasReadPermission(path string) bool {
+	file, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+	return true
+}
+
+// isValidFormat checks if the given format is present in the list of supported formats.
+// Returns true if the format is supported, otherwise returns false.
+
+func isValidFormat(format string, supportedFormats []string) bool {
+	for _, supportedFormat := range supportedFormats {
+		if format == supportedFormat {
+			return true
+		}
+	}
+	return false
+}
